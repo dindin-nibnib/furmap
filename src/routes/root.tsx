409cd7c8c056fd1231002f3f9b4aae98ce98987e
Odin Beuchat <odin.beuchat@divtec.ch>
2022-11-22 16:10:22 +0100

    created header

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,10 +1,37 @@
-
+import markerSmall from "../assets/marker_small.png";
+import { Outlet, Link } from "react-router-dom";
 
 const Root = () => {
 	return(
-		<div>
-			hello world
-		</div>
+		<>
+			<header>
+				<img src={markerSmall} alt="Furmap's marker" />
+				<h1>Furmap</h1>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Map</Link>
+						</li>
+						<li>
+							<Link to="/info">Info</Link>
+						</li>
+						<li>
+							<Link to="/discord">Discord</Link>
+						</li>
+						<li>
+							<Link to="/geo">Geo</Link>
+						</li>
+						<li>
+							<Link to="https://twitter.com/Furmap_net">Twitter</Link>
+						</li>
+				</ul>
+				</nav>
+			</header>
+
+			<main>
+				<Outlet/>
+			</main>
+		</>
 	)
 }
 
