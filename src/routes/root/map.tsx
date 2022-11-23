74fbab4a8c0e7b4a8a469280131f769f473fdbf3
Odin Beuchat <odin.beuchat@divtec.ch>
2022-11-23 10:13:29 +0100

    fixed styles

--- src/routes/root/map.tsx
+++ src/routes/root/map.tsx
@@ -14,21 +14,12 @@ const Map = () => {
 	});
 	return (
 		<>
-			{/*<MapContainer
-			center={[51.505, -0.09]}
-			zoom={13}
-			scrollWheelZoom={false}
-		>
-			<TileLayer
-				url="https://{w}.basemaps.cartocdn.com/dark_all/{x}/{y}/{z}.png"
-			/>
-	</MapContainer>*/}
-			<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{height:"100%"}}>
+			<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "calc(100vh - 56px)" }}>
 				<TileLayer
 					attribution={'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
 					url={"https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"}
 				/>
-				
+
 				<Marker position={[51.505, -0.09]} icon={icon}>
 					<Popup>
 						A pretty CSS3 popup. <br /> Easily customizable.
