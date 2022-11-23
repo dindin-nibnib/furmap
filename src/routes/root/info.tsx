6e6d65cb6034c8def1e901445fbfd440fba2d543
Odin Beuchat <odin.beuchat@divtec.ch>
2022-11-23 08:33:17 +0100

    added a space

--- src/routes/root/info.tsx
+++ src/routes/root/info.tsx
@@ -1,15 +1,14 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Info = () => {
 	return (
-	<>
-		<h2>Info</h2>
-		<p>Furmap has been existing for more than 3 years.</p>
+		<>
+			<h2>Info</h2>
+			<p>Furmap has been existing for more than 3 years.</p>
 
-		<p>This website fist was created by {"<creator name>"}, and is now maintained by 
-		<Link to="https://dindin.ch">dindin</Link>.</p>
-	</>
-	)
-}
+			<p>This website fist was created by {"<creator name>"}, and is now maintained by <Link to="https://dindin.ch">dindin</Link>.</p>
+		</>
+	);
+};
 
-export default Info
+export default Info;
