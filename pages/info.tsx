4a71c9fbdfc241a9c7299f60af525ef8811d2e6e
Odin Beuchat <odin.beuchat@divtec.ch>
2022-11-23 22:10:21 +0100

    added credit

--- pages/info.tsx
+++ pages/info.tsx
@@ -6,7 +6,11 @@ const Info = () => {
 			<h2>Info</h2>
 			<p>Furmap has been existing for more than 3 years.</p>
 
-			<p>This website fist was created by {"<creator name>"}, and is now maintained by <Link href={"https://dindin.ch"}>dindin</Link>.</p>
+			<p>
+				This website is not the official one!
+				It's a fork of the original, but written with nextjs, and is maintained by <Link href={"https://dindin.ch"}>dindin</Link>.
+				The original website can be found here: <Link href={"https://furmap.net"}>furmap.net</Link>.
+			</p>
 		</>
 	);
 };
