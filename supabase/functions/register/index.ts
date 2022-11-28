f036fa073c1dd618365202ac55e1799153a135d4
Odin Beuchat <odin.beuchat@divtec.ch>
2022-11-28 16:50:07 +0100

    added  checks

--- supabase/functions/register/index.ts
+++ supabase/functions/register/index.ts
@@ -3,27 +3,49 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from "http://deno.land/std@0.131.0/http/server.ts";
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import crypto from "https://esm.sh/crypto@1.0.1";
+import { MailtrapClient } from "https://esm.sh/mailtrap@3.0.1";
 
 serve(async (req) => {
+	const { url, method } = req;
 	const supabaseUrl = Deno.env.get("SUPABASE_URL") || "";
 	const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY") || "";
 
 	const supabase = createClient(supabaseUrl, supabaseAnonKey, { db: { schema: "public" } });
-	const { data } = await supabase.from('markers').select('name');
+	const body = await req.json();
 
-	const res = {
-		message: `Hello ${data[0].name}!`,
-	};
+	const taskPattern = new URLPattern({ pathname: '/restful-tasks/:id' });
+	const matchingPath = taskPattern.exec(url);
+	const id = matchingPath ? matchingPath.pathname.groups.id : null;
 
-	return new Response(
-		JSON.stringify(res),
-		{ headers: { "Content-Type": "application/json" } },
-	);
+	if (id === "send") {
+		const { email, name, lat, lng, captcha } = body;
+
+		if (!email || !captcha || !name || !lat || !lng) {
+			return new Response(JSON.stringify({
+				message: "Unproccesable request, please provide the required fields",
+			}), {
+				status: 422,
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+		}
+
+		const { data } = await supabase.from('markers').select('name').eq('email', email);
+		if (data.length > 0) {
+			return new Response(JSON.stringify({ error: "Email already exists" }), {
+				status: 400,
+				headers: {
+					"content-type": "application/json",
+				},
+			});
+		}
+
+		return new Response(
+			JSON.stringify(res),
+			{ headers: { "Content-Type": "application/json" } },
+		);
+	}
 });
-
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
