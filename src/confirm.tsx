4e623783750f55da57dd281b76b01c486acfe380
Odin Beuchat <odin.beuchat@divtec.ch>
2022-11-29 12:52:42 +0100

    added error message

--- src/confirm.tsx
+++ src/confirm.tsx
@@ -5,22 +5,25 @@ import { useLocation } from "react-router-dom";
 const Confirm = () => {
 	const route = useLocation();
 	const [confirmed, setConfirmed] = useState<boolean | null>(null);
+	const [error, setError] = useState<string | null>(null);
 	useEffect(() => {
-		(
-			async () => {
-				const response = await fetch(`https://${import.meta.env.VITE_PROJECT_ID}.functions.supabase.co/register/verify${route.search}`, {
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json",
-						"Access-Control-Request-Method": "GET",
-						"Authorization": "Bearer " + import.meta.env.VITE_SUPABASE_ANON_KEY,
-						"Access-Control-Request-Headers": "Content-Type, Authorization"
-					},
-				});
-
-				setConfirmed(response.status === 200);
+		console.log("effect!");
+		fetch(`https://${import.meta.env.VITE_PROJECT_ID}.functions.supabase.co/register/verify${route.search}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				"Access-Control-Request-Method": "GET",
+				"Authorization": "Bearer " + import.meta.env.VITE_SUPABASE_ANON_KEY,
+				"Access-Control-Request-Headers": "Content-Type, Authorization"
+			},
+		}).then(async (response) => {
+			if (response.status === 200) {
+				setConfirmed(true);
+			} else {
+				setConfirmed(false);
+				setError((await response.json()).message);
 			}
-		)();
+		});
 	}, []);
 
 	return (
@@ -38,6 +41,7 @@ const Confirm = () => {
 						:
 						<div className="flex flex-col items-center justify-center">
 							<h1 className="text-3xl font-bold">There was an error verifying your account.</h1>
+							<p className="text-xl">{error}</p>
 						</div>
 			}
 		</div>
